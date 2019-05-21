a1053caee7787bea6c83fb4c8b20f77504804bc2
NwosuCC <nwosu.cyprian.c@gmail.com>
2019-05-21 02:15:24 +0100

    switched to redis session

--- config/database.php
+++ config/database.php
@@ -1,5 +1,29 @@
 <?php
 
+$ports = [
+  'mysql' => '3306',
+  'pgsql' => '',
+  'sqlsrv' => '1433',
+];
+
+// Server DATABASE_URL
+$db_url = ($db_url = env('DATABASE_URL')) ? parse_url( $db_url ) : null;
+
+$db_host = $db_url ? $db_url['host']  : env('DB_HOST', '127.0.0.1');
+$db_port = $db_url ? $db_url['port']  : env('DB_PORT', '');
+$db_name = $db_url ? $db_url['host']  : env('DB_DATABASE', 'forge');
+$db_user = $db_url ? $db_url['user']  : env('DB_USERNAME', 'forge');
+$db_pass = $db_url ? $db_url['pass']  : env('DB_PASSWORD', '');
+
+
+// Server REDIS_URL
+$redis_url = ($redis_url = env('REDIS_URL')) ? parse_url( $redis_url ) : null;
+
+$redis_host = $redis_url ? $redis_url['host']  : env('REDIS_HOST', '127.0.0.1');
+$redis_port = $redis_url ? $redis_url['port']  : env('REDIS_PORT', 6379);
+$redis_pass = $redis_url ? $redis_url['pass']  : env('REDIS_PASSWORD', null);
+
+
 return [
 
     /*
@@ -42,11 +66,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $db_host,
+            'port' => $db_port ?: '3306',
+            'database' => $db_name,
+            'username' => $db_user,
+            'password' => $db_pass,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -58,11 +82,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $db_host,
+            'port' => $db_port ?: '5432',
+            'database' => $db_name,
+            'username' => $db_user,
+            'password' => $db_pass,
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -72,11 +96,11 @@ return [
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $db_host ?: 'localhost',
+            'port' => $db_port ?: '1433',
+            'database' => $db_name,
+            'username' => $db_user,
+            'password' => $db_pass,
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -112,17 +136,21 @@ return [
 
         'client' => 'predis',
 
+        'options' => [
+          'cluster' => env('REDIS_CLUSTER', 'predis'),
+        ],
+
         'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
+            'host' => $redis_host,
+            'password' => $redis_pass ?: null,
+            'port' => $redis_port ?: 6379,
+            'database' => env('REDIS_redis', 0),
         ],
 
         'cache' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
+            'host' => $redis_host,
+            'password' => $redis_pass ?: null,
+            'port' => $redis_port ?: 6379,
             'database' => env('REDIS_CACHE_DB', 1),
         ],
 
