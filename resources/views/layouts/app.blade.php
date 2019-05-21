c64fca42c51eb4db3c687f8921fda10f0429494e
NwosuCC <nwosu.cyprian.c@gmail.com>
2019-05-21 22:49:01 +0100

    fixed house auto-attach to creator

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
+    <script src="{{ asset('js/jquery-1.9.1.min.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
