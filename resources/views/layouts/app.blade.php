aef4d21fb7217f14b5996e265a8d9043446213a3
NwosuCC <nwosu.cyprian.c@gmail.com>
2019-05-27 09:44:04 +0100

    app.css fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     {{--@stack('actions-scripts')--}}
-    <script src="{{ asset('js/modal-scripts.js') }}" defer></script>
+{{--    <script src="{{ asset('js/modal-scripts.js') }}" defer></script>--}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
